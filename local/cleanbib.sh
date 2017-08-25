62f5e9e8806bdc87c6634842a196adb0cd740478
fuxialexander <fuxialexander@gmail.com>
2017-08-26 04:41:55 +0800

    Add: mail snipptes; cleanbib

---
+++ local/cleanbib.sh
@@ -0,0 +1,3 @@
+sed -i "" "s,local-url\ =\ {file:\/\/localhost,file\ =\ {file\.pdf:,g" /Users/xfu/Dropbox/org/ref.bib
+# sed -i "" "s,.*\\url.*,,g " /Users/xfu/Dropbox/org/ref.bib
+sed -i "" "s,\.pdf},\.pdf:application\/pdf},g" /Users/xfu/Dropbox/org/ref.bib
